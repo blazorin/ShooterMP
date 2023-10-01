08d07a213332457964367d4e8a3f1782faa0e451
blazorin <alberto@darksnakegames.com>
2023-10-01 17:33:03 +0200

    Initial commit

---
+++ Source/ShooterMP/ShooterMPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterMPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERMP_API AShooterMPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
